df9807a8063c6e9946490f35599a81b61c2f68c3
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-07 21:44:09 +0900

    move style to same file

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/Errors/error-overlay-pagination/error-overlay-pagination.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/Errors/error-overlay-pagination/error-overlay-pagination.tsx
@@ -1,5 +1,7 @@
 import type { ReadyRuntimeError } from '../../../helpers/get-error-by-type'
+
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { noop as css } from '../../../helpers/noop-template'
 import { LeftArrow } from '../../../icons/LeftArrow'
 import { RightArrow } from '../../../icons/RightArrow'
 
@@ -149,3 +151,56 @@ export function ErrorOverlayPagination({
     </div>
   )
 }
+
+export const styles = css`
+  .error-overlay-pagination {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: var(--size-1);
+    gap: var(--size-2);
+    background: var(--color-background-100);
+    box-shadow: var(--shadow-sm);
+
+    border: 1px solid var(--color-gray-400);
+    border-radius: var(--rounded-full);
+  }
+
+  .error-overlay-pagination-count {
+    color: var(--color-gray-900);
+    text-align: center;
+    font-size: var(--size-font-small);
+    font-weight: 500;
+    line-height: var(--size-4);
+  }
+
+  .error-overlay-pagination-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: var(--size-1);
+    background: var(--color-gray-300);
+
+    border: none;
+    border-radius: var(--rounded-full);
+
+    &:focus {
+      outline: none;
+    }
+
+    &:not(:disabled):active {
+      background: var(--color-gray-500);
+    }
+
+    &:disabled {
+      opacity: 0.5;
+      cursor: not-allowed;
+    }
+  }
+
+  .error-overlay-pagination-button-icon {
+    color: var(--color-gray-1000);
+  }
+`
