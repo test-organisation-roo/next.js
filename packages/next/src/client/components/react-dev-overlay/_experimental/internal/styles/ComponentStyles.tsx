e9e70a987d930dc7895b2a92f9dc6c2f17e8aafe
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-09 05:23:29 +0900

    call stack

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/styles/ComponentStyles.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/styles/ComponentStyles.tsx
@@ -11,12 +11,16 @@ import { styles as versionStaleness } from '../components/VersionStalenessInfo/V
 import { styles as buildErrorStyles } from '../container/BuildError'
 import { styles as containerErrorStyles } from '../container/Errors'
 import { styles as containerRuntimeErrorStyles } from '../container/RuntimeError'
+import { COPY_BUTTON_STYLES } from '../components/copy-button'
+import { CALL_STACK_FRAME_STYLES } from '../components/call-stack-frame/call-stack-frame'
 import { noop as css } from '../helpers/noop-template'
 
 export function ComponentStyles() {
   return (
     <style>
       {css`
+        ${COPY_BUTTON_STYLES}
+        ${CALL_STACK_FRAME_STYLES}
         ${overlay}
         ${toast}
         ${dialog}
